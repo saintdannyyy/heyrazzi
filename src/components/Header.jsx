3f7c59abb3503de394562788ef489603fe5c8e2f
Saintdannyyy <danieltesla746@gmail.com>
2025-02-15 16:06:40 +0000

    Refactor Header component for improved layout and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import { ArrowCircleUpRight } from "@untitled-ui/icons-react";
 
 const Header = () => {
   return (
-    <header className="h-15 mt-2 bg-[#FFFAF6]">
+    <header className="top-0 h-15 mt-2 bg-[#FFFAF6]">
       <div className="bg-[#F0EDEA] mx-auto w-[90%] md:w-[80%] p-2 rounded-lg flex items-center justify-between">
         {/* Logo */}
           <div className="flex items-center">
-            <a href="/" aria-label="Homepage" className="inline-block h-10 w-24">
+            <a href="/" aria-label="Homepage" className="inline-block h-10 w-24 border-0">
               <img src={logo} alt="HeyRazzi Logo" className="h-full w-full" />
             </a>
             {/* Navigation */}
           <nav className="hidden md:flex items-center">
-          <span className="pl-3 text-gray-600">|</span>
+          <span className="pl-3 text-gray-400">|</span>
             <a href="/about" className="text-gray-700 hover:text-black font-bold transition-colors duration-200"
             >
               About
@@ -34,17 +34,17 @@ const Header = () => {
         </div>
 
         {/* Buttons */}
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 mr-0">
           <Button
             variant="outline"
-            className="border-orange-400 text-orange-500 hover:bg-orange-100 transition-colors duration-200"
+            className="w-20 border-orange-400 text-orange-500 hover:bg-orange-100 hover:text-orange-700 transition-colors duration-200"
           >
             Log In
           </Button>
-          <Button className="bg-orange-500 text-white hover:bg-orange-600 hover:text-orange-600 flex items-center space-x-2 transition-colors duration-200">
+          <a href="/signup"><Button  className="w-36 bg-orange-500 text-white hover:bg-orange-600 hover:text-orange-600 flex items-center space-x-2 transition-colors duration-200">
             <span>Create Account</span>
             <ArrowCircleUpRight/>
-          </Button>
+          </Button></a>
         </div>
       </div>
     </header>
