b98e94283bed70f187a49d580f606af6ad71a1c5
Saintdannyyy <danieltesla746@gmail.com>
2025-02-15 16:07:32 +0000

    Refactor SignUp component layout and styling for improved user experience

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,48 +1,72 @@
-import React from 'react'
-import Heyrazzi from '../assets/logo.png'
-import side from '../assets/side.png'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { ArrowCircleUpRight } from '@untitled-ui/icons-react'
-import Footer from '@/components/Footer'
+import React from "react";
+import Heyrazzi from "../assets/logo.png";
+import side from "../assets/side.png";
+import logo from "@/assets/logo.png";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowCircleUpRight } from "@untitled-ui/icons-react";
+import Footer from "@/components/Footer";
 
 const SignUp = () => {
-    return (
+  return (
+    <div className="mx-32 mt-5">
+      <a
+        href="/"
+        aria-label="Homepage"
+        className="inline-block h-10 w-24 border-0"
+      >
+        <img src={logo} alt="HeyRazzi Logo" className="h-full w-full" />
+      </a>
+      <div className="flex h-screen mt-20">
         <div>
-            <div className="flex">
-                <img src={Heyrazzi} alt="Heyrazzi" className="w-1/2" />
+          <h1 className="text-8xl font-semibold">
+            Join <br />
+            HeyRazzi
+          </h1>
+          <p className="text-gray-400 mt-2 pl-[20%]">Sign up for free!</p>
+          <form>
+            <div className="flex mt-2">
+              <Input
+                placeholder="Name"
+                className="mr-2 rounded-lg bg-[#D1D1D1] w-1/2 p-6"
+              />
+              <Input
+                placeholder="Email"
+                className="mr-2 rounded-lg bg-[#D1D1D1] w-1/2 p-6"
+              />
             </div>
-            <div className="flex justify-center items-center h-screen mt-10">
-                <div>
-                    <h1 className="text-2xl font-bold">Join <br />HeyRazzi</h1>
-                    <p className='text-gray-400'>Sign up for free!</p>
-                    <form>
-                        <div className='flex mt-2'>
-                            <Input placeholder='Name' className='mr-2 rounded-3xl bg-[#D1D1D1] w-20' />
-                            <Input placeholder='Email' className='mr-2 rounded-3xl bg-[#D1D1D1] w-20' />
-                        </div>
-                        <div className='flex mt-2'>
-                            <Input placeholder='Enter your username' className='rounded-3xl bg-[#D1D1D1] w-40' />
-                            <Input placeholder='Password' className='mt-2 rounded-3xl bg-[#D1D1D1] w-40' />
-                            <Button className='mt-2 w-40 p-5'>Create Account <ArrowCircleUpRight stroke='1' color='white' /></Button>
-                        </div>
-                    </form>
-                    <div className='mt-5'>
-                        <p>Already have an account? <a href='/login' className='text-orange-500'>Login</a></p>
-                    </div>
-                </div>
+            <div className="mt-3">
+              <Input
+                placeholder="Enter your username"
+                className="rounded-lg bg-[#D1D1D1] p-6"
+              />
+              <Input
+                placeholder="Password"
+                className="mt-3 rounded-lg bg-[#D1D1D1] p-6"
+              />
+              <Button className="mt-2 p-7 bg-orange-500">
+                Create Account <ArrowCircleUpRight stroke="1" color="white" />
+              </Button>
             </div>
-            <div className="flex ml-10">
-                <img
-                    src={side}
-                    alt="Side Image"
-                    className="max-w-full h-auto"
-                />
-            </div>
-            <div className='mt-10'>
-                <Footer/>
-                </div>
+          </form>
+          <div className="mt-5 text-center text-2xl font-semibold">
+            <p>
+              Already have an account?{" "}
+              <a href="/login" className="text-orange-500 font-bold">
+                Login
+              </a>
+            </p>
+          </div>
         </div>
-    )
-}
-export default SignUp
+        <div className="flex">
+          <img src={side} alt="Side Image" className="max-w-full h-full" />
+        </div>
+      </div>
+
+      <div className="mt-12">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+export default SignUp;
